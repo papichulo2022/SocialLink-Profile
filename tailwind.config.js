bc6bb0febac7be04add6dd7f26b419c0afb3b64b
papichulo2022 <140461541+papichulo2022@users.noreply.github.com>
2024-08-10 18:05:21 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+      'cgray': '#1f1f1f',
+    },
+    },
+  },
+  plugins: [],
+}
+
